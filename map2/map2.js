8fc4227038c08cf4be00ef978409ffc61e5da553
Jiyoung <46572768+Jiyoung0915@users.noreply.github.com>
2019-04-02 09:31:23 -0500

    Add files via upload

--- map2/map2.js
+++ map2/map2.js
@@ -8,11 +8,13 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z
 // load a latest gridded surface oceanographic forecast model guidance and immediate shelf from the NOAA/NOS San Francisco Bay Operational Forecast System (SFBOFS).
 let sanFran = L.esri.dynamicMapLayer({
   url: 'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/guidance_model_coastalocean_sfbofs_time/MapServer',
-  opacity: 0.9
+  opacity: 0.9,
+  attirbution: 'NOAA'
 }).addTo(oceanActivities)
 
 // load a latest gridded surface oceanographic forecast model guidance and immediate shelf from the NOAA/NOS Northern Gulf of Mexico Bay Operational Forecast System (SFBOFS).
 let northernGulf = L.esri.dynamicMapLayer({
   url: 'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/guidance_model_coastalocean_ngofs_time/MapServer',
-  opacity: 0.9
+  opacity: 0.9,
+  attirbution: 'NOAA'
 }).addTo(oceanActivities)
